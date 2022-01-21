d8770af1fc83e931e5a8f09fec2eea670a99a100
Username <igorgunov1564@yandex.ru>
2022-01-21 12:15:21 +0300

    удрал лишнии методы

--- src/Task/Epic.java
+++ src/Task/Epic.java
@@ -35,18 +35,6 @@ public class Epic {
         return status;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setStatus(String status) {
         this.status = status;
     }
--- src/Task/Task.java
+++ src/Task/Task.java
@@ -29,18 +29,6 @@ public class Task {
         return status;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setStatus(String status) {
         this.status = status;
     }
